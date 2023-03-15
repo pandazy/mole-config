46bf44d5fdbea23c16d75fa1c6878d9dcbf9aa53
Martin Zheng <jszhengyq@gmail.com>
2023-03-15 08:25:11 -0700

    feat(typescript): added babel config

---
+++ babel.config.js
@@ -0,0 +1,18 @@
+"use strict";
+
+module.exports = {
+  presets: [
+    "@babel/preset-env",
+    "@babel/preset-typescript"
+  ],
+  plugins: [
+    [
+      "module-resolver",
+      {
+        alias: {
+          "@root": "./src",
+        },
+      },
+    ],
+  ],
+};
