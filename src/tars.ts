39cdd01bae45d7d8aa78900b252d4ec8913ff2d3
Martin Zheng <jszhengyq@gmail.com>
2023-03-18 21:00:20 -0700

    fix(tar): updated untar command

--- src/tars.ts
+++ src/tars.ts
@@ -26,8 +26,8 @@ export function tar() {
 export function untar(projectType: ProjectType, force = false) {
   const tarName = `${projectType}.tar.gz`;
   const srcConfigPath = getVendorPath('dist', tarName);
-  const options = force ? 'xzrf' : 'xzkf';
-  execSync(`tar -${options} ${srcConfigPath} -C ${getUserPath('.')}`, {
+  const overwriteFlag = force ? '--overwrite' : '';
+  execSync(`tar ${overwriteFlag} -xzf ${srcConfigPath} -C ${getUserPath('.')}`, {
     stdio: 'inherit',
   });
 }
