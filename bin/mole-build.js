8e0c593e027aefd4eb1aa8958a16f5ff122aeb0e
Martin Zheng <jszhengyq@gmail.com>
2023-03-22 07:59:17 -0700

    fix(mole-build): added path alias update before running the build

--- bin/mole-build.js
+++ bin/mole-build.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 
 const { execSync } = require('child_process');
+const { default: updateTsConfigPaths } = require('../dist/update-tsconfig-paths');
 
+updateTsConfigPaths();
 execSync([
   'rm -rf dist',
   'yarn tsc --build tsconfig-build.json',
