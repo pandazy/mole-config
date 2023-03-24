505b689a88967ed400a56157a009fad3ff238be5
Martin Zheng <jszhengyq@gmail.com>
2023-03-23 21:47:43 -0700

    fix(path-alias): added @pandazy/path-alias as dev dependency

--- src/dep-install.ts
+++ src/dep-install.ts
@@ -16,6 +16,7 @@ const DepsMap: Record<DepType, string[]> = {
     '@babel/preset-env',
     '@babel/preset-typescript',
     'babel-plugin-module-resolver',
+    '@pandazy/path-alias',
   ],
   jest: ['@types/jest', 'jest', 'ts-jest'],
   typescript: [
