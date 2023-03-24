eedd59bdf73e918757de10f9e6234fcefc0301f7
Martin Zheng <jszhengyq@gmail.com>
2023-03-24 08:39:27 -0700

    fix(dep)!: removed mole-config as a common dev-dep because it can be run by npx

--- src/dep-install.ts
+++ src/dep-install.ts
@@ -9,7 +9,7 @@ export type DepType =
   | 'gitHooks';
 
 const DepsMap: Record<DepType, string[]> = {
-  mole: ['@pandazy/mole-config', '@pandazy/mole-scripts'],
+  mole: ['@pandazy/mole-scripts'],
   babel: [
     '@babel/cli',
     '@babel/core',
